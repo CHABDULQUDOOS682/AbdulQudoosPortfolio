9063f227918e8fd7394e214f6aca960d5b246ae7
CHABDULQUDOOS682 <abdulqudoos682@gmail.com>
2025-03-30 01:32:51 +0500

    Fix mailer

--- js/config.js
+++ js/config.js
@@ -1,7 +1,6 @@
-const emailJSConfig = {
-    PUBLIC_KEY: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
-    SERVICE_ID: import.meta.env.VITE_EMAILJS_SERVICE_ID,
-    TEMPLATE_ID: import.meta.env.VITE_EMAILJS_TEMPLATE_ID
-};
 
-export default emailJSConfig;
+const emailJSConfig = {
+    PUBLIC_KEY: "",
+    SERVICE_ID: "",
+    TEMPLATE_ID: ""
+};
